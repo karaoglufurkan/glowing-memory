db9d7744b0670af45d12261a66e284ce70a75f6f
Furkan Karaoglu <karaoglufurkan@outlook.com>
2020-05-20 22:35:39 +0300

    source and target paths are parameterized

--- categorization.py
+++ categorization.py
@@ -1,31 +1,28 @@
 import os
 import json
 from shutil import copyfile
+from shutil import move
 
 class Categorization:
-    def __init__(self, ):
+    def __init__(self, source_path, target_path):
         self.config = self.get_config()
-        self.main_directory = os.getcwd() + "/demo_folder"
+        self.source_path = source_path
+        self.target_path = target_path
 
     def create_folders(self,):
-        [os.mkdir(f) for f in self.config]
+        [os.mkdir(os.path.join(self.target_path, f)) for f in self.config]
 
     def get_config(self,):
         with open("config.json", "r") as config_reader:
             return json.loads(config_reader.read())
 
     def categorize(self,):
-        for walk in os.walk(self.main_directory):
+        for walk in os.walk(self.source_path):
             for file_name in walk[2]:
-                copyfile(str(walk[0]) + "/" + str(file_name), str(os.getcwd()) + "/" + str(self.find_folder_name(file_name)) + "/" + str(file_name))
-
+                # copyfile(os.path.join(walk[0], file_name), os.path.join(self.target_path, self.find_folder_name(file_name), file_name))
+                move(os.path.join(walk[0], file_name), os.path.join(self.target_path, self.find_folder_name(file_name)))
     def find_folder_name(self, file_name):
         for i in self.config:
             for j in self.config[i]:
                 if file_name.endswith(str(j)):
-                    return i
-
-if __name__ == "__main__":
-    cat = Categorization()
-    # cat.create_folders()
-    cat.categorize()    
+                    return i   
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+
 from categorization import Categorization
 
-cat = Categorization()
+cat = Categorization(os.path.join(os.getcwd(), "source_folder"), os.path.join(os.getcwd(), "target_folder"))
 
 cat.create_folders()
 cat.categorize()
